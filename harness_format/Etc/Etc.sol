4d5deef23510753ef6e4876c44ce297c3cec1db8
sam bacha <sam@manifoldfinance.com>
2022-07-20 17:03:17 -0700

    feat: test harness and format

---
+++ harness_format/Etc/Etc.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.24;
+
+// line comment
+
+/*
+	Block comment
+*/
+
+contract Contract {
+    struct StructWithFunctionTypes {
+        function (uint, uint) returns(bool) a;
+        function (uint, uint) returns(bool,bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, bytes32)
+            a;
+        function(bytes32, bytes32)   internal view[] b;
+        function  (bytes32, bytes32)internal[] c;
+        function(bytes32, bytes32, bytes32, bytes32, bytes32, bytes32)   internal view[]
+            d;
+        function(bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, bytes32)   internal view[]
+            e;
+    }
+
+    event SomeEvent();
+
+    modifier modifierWithoutParams() {
+        require(msg.sender != address(0));
+        _;
+    }
+
+    modifier modifierWithParams(address x) {
+        require(msg.sender != x);
+        _;
+    }
+
+    function ifBlockInOneLine(uint256 a) returns (uint256 b) {
+        if (a < 0) b = 0x67; else if (a == 0) b = 0x12; else b = 0x78;
+    }
+
+    function forWithoutBlock() {
+        uint256 i;
+        uint256 sum;
+        for (i = 0; i < 10; i++) sum += i;
+    }
+
+    function fun(uint256 a) returns (uint256) {
+        if (something) foo();
+        // comment
+        else if (somethingElse) bar(); else whatever();
+        return;
+    }
+}
